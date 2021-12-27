45a0c91b484519d19e2f847fc586236accaf1ecd
veni1111 <selvaveni1991@gmail.com>
2021-12-27 12:02:09 +0000

    final

---
+++ India.java
@@ -0,0 +1,13 @@
+public class india
+{
+public static void main(string[] args)
+{
+	//age of citizen is 6
+	int age = 6;
+//object creation
+india citizen=new india();
+system.out,println(citizen.age);//age of citizen
+//dynamic binding
+//new-->allocates memory
+}
+}
