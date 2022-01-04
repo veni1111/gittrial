700a47569f03d76a45d0ed7543eb1b281874a9d2
veni1111 <selvaveni1991@gmail.com>
2022-01-04 20:00:28 +0000

    final

---
+++ Employee.java
@@ -0,0 +1,12 @@
+package tamilnadu.chennai;
+
+public class Employee //canonical
+{
+public static void main(String[] args)
+{
+Office off = new Office();
+	System.out.println(off.travel);
+	off.providesnacks();
+
+}
+}
