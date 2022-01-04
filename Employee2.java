ae7fce0cc9d4d1be4688798180d713eb613a02ef
veni1111 <selvaveni1991@gmail.com>
2022-01-04 20:02:01 +0000

    final

---
+++ Employee2.java
@@ -0,0 +1,20 @@
+package tamilnadu.madurai;
+
+import tamilnadu.chennai.Office;
+
+public class Employee2 //canonical
+{
+public static void main(String[] args)
+{
+Employee2 emp = new Employee2();
+	emp.wfh();
+	Office off = new Office();
+	off.providesalary();
+
+}
+	public void wfh()
+	{
+		System.out.println("connected through online");
+		}
+}
+
