4f9354f6b8a4ebcf4e65e905364d443b97bd8477
veni1111 <selvaveni1991@gmail.com>
2022-01-07 21:15:30 +0000

    final

---
+++ Child.java
@@ -0,0 +1,16 @@
+package chennai;
+import hometown.Parent;
+public class Child extends Parent
+{
+
+public static void main(String[] args)
+{
+	
+Child child = new Child();
+System.out.println(child.name);
+child.call();
+	
+}
+}
+
+
