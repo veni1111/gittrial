de9dff25ab2e1095f430b0c807dc17c5e18c62f4
veni1111 <selvaveni1991@gmail.com>
2022-01-07 21:03:00 +0000

    final

---
+++ Samsung.java
@@ -0,0 +1,23 @@
+package korea.south;
+
+public class Samsung
+{
+	protected int pinNo = 1234;
+	protected void changepin()
+	{
+		System.out.println("changing pin");
+	}
+	private void reserch()
+	{
+		System.out.println("research");
+		}
+			
+public void makequality()
+{
+System.out.println("samsung");
+}
+public void givewarranty()
+{
+System.out.println("warranty");
+}
+}
