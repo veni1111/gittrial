5cb43a071b1f97dd6fc456cd0e83c5c95128dc54
veni1111 <selvaveni1991@gmail.com>
2022-01-07 21:10:12 +0000

    final

---
+++ Desktop2.java
@@ -0,0 +1,15 @@
+package india.south;
+import korea.south.Samsung;
+public class Desktop2 extends Samsung
+{
+public static void main(String[] args)
+{
+Desktop2 dt = new Desktop2();
+dt.makequality();
+dt.givewarranty();
+//dt.research();
+System.out.println(dt.pinNo);
+dt.changepin();
+
+}
+}
