e81c67703e5f4ae98be61ac64003e28c29fc3423
veni1111 <selvaveni1991@gmail.com>
2022-01-07 21:13:15 +0000

    final

---
+++ Parent.java
@@ -0,0 +1,12 @@
+package hometown;
+public class Parent
+{
+protected String name = "Baskar";
+public static void main(String[] args)
+{
+}
+protected void call()
+{
+System.out.println("Hello" + name);
+}
+}
