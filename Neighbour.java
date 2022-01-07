8f506519096351b2753f36dceb90a245582e1871
veni1111 <selvaveni1991@gmail.com>
2022-01-07 21:14:21 +0000

    final

---
+++ Neighbour.java
@@ -0,0 +1,18 @@
+package hometown;
+public class Neighbour
+{
+
+public static void main(String[] args)
+{
+	
+	Parent parent = new Parent();
+	System.out.println(parent.name);
+parent.call();
+	
+}
+}
+
+
+
+
+
